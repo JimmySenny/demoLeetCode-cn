bcd1d607c6e56e18bb5641a53099ce851d1b82d0
JimmySenny <jimmysenny@126.com>
2020-03-26 09:23:33 +0800

    945

--- c&cc/945.c
+++ c&cc/945.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
 
+#include <stdio.h>
+#include <stdlib.h>
+
+int compi(const void *a, const void *b) {
+	const int *p = a;
+	const int *q = b;
+    
+	return *p - *q;
+}
+
 void mysort(int * A, int ASize){
     int i = 0;
     int j = 0;
@@ -14,22 +24,53 @@ void mysort(int * A, int ASize){
             }
         }
     }
+    
+}
+
+void mysort2(int * A, int ASize, int start, int end){
+    int i = start;
+    int j = 0;
+    int tmp = 0;
+    if( end >= ASize){
+        return ;
+    }
+    
+    for( i; i<ASize; i++){
+        j = i+1;
+        for(j;j<ASize; j++){
+            if(A[i] > A[j] ){
+                tmp = A[i];
+                A[i] = A[j];
+                A[j] = tmp;
+            }
+        }
+    }
 }
 
 int minIncrementForUnique(int* A, int ASize){
     int minMove = 0;
-    mysort(A,ASize);
+    
+    if(ASize < 2){
+        return minMove;
+    }
+    
+    //mysort(A,ASize);
+    qsort(A,ASize,sizeof(A[0]),compi);
+    
 
-    for (int i = 0; i < ASize - 1; i++ ){
+    for (int i = 0; i < ASize - 2; i++ ){
         while (A[i] == A[i+1]) {
             A[i+1]++;
             minMove++;
-            mysort(A,ASize);
+            mysort2(A,ASize,i, i+2);
         }
+    }
+    
+    if( A[ASize-2] == A[ASize-1]){
+        minMove++;
     }
 
     return minMove;
-
 }
 
 int main( int argc, char[] argv ){
@@ -37,3 +78,4 @@ int main( int argc, char[] argv ){
     int b = minIncrementForUnique(a, 5);
     printf("[%d]", b);
 }
+
