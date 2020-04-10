8abbac430ecec1e7b2d3dfe9e76e45ca29719a85
jimmysenny <jimmysenny@126.com>
2020-04-10 19:26:35 +0800

    192

--- 192/192.sh
+++ 192/192.sh
@@ -2,3 +2,6 @@
 
 
 cat words.txt  | tr ' ' '\n' | sed /^$/d | sort | uniq -c
+cat words.txt   | tr '\n' ' ' | awk '{ for(i=1;i<=NF;i++){ sum[$i]++}}END {for (j in sum ) print j " " sum[j]}' 
+cat words.txt  | xargs -n 1 | sort | uniq -c
+
