9cfbacc99479a1f30d96042f6bd9a250a8309d99
jimmysenny <jimmysenny@126.com>
2020-04-23 18:38:15 +0800

    add rotate

---
+++ algorithm-initial/gcd.c
@@ -0,0 +1,145 @@
+//辗转相除法 
+
+ int gcd(int a,int b)
+
+ { 
+     if(a%b==0)
+     return b;
+         else;
+         return gcd(b,a%b);
+ }
+
+
+//穷举法
+
+ int divisor (int a, int b) //自定义函数求两数的最大公约数
+
+ {
+      int  temp;//定义整型变量
+     temp=(a>b)?b:a;//采种条件运算表达式求出两个数中的最小值
+     while(temp>0)
+
+      {
+              if(a%temp==0&&b%temp==0)//只要找到一个数能同时被a,b所整除，则中止循环
+              break;
+              temp--;//如不满足if条件则变量自减，直到能被a,b所整除 
+      } 
+     return (temp);//返回满足条件的数到主调函数处 
+  } 
+
+//更相减损法
+ int gcd2(int m,int n)
+ {
+     int i=0,temp,x;
+     while(m%2==0&&n%2==0)//判断m和n能被多少个2整除
+    {
+         m/=2;
+         n/=2;
+         i+=1;
+    } 
+     if(m<n)//m保存大的值
+   {
+        temp=m;
+        m=n;
+        n=temp;
+   } 
+     while(x)
+   {
+        x=m-n;
+        m=(n>x)?n:x;
+        n=(n<x)?n:x;
+        if(n==(m-n))
+        break;
+   }
+    if(i==0)
+    return n;
+        else
+        return (int) pow(2,i)*n;
+  } 
+
+  //Stein算法
+   int Stein( unsigned int x, unsigned int y )
+  /* return the greatest common divisor of x and y */
+   {
+        int factor = 0;
+        int temp;
+        if ( x < y )
+        {
+                temp = x;
+                x = y;
+                y = temp;
+        }
+        if ( 0 == y )
+        {
+                return 0;
+        }
+        while ( x != y )
+        {
+                if ( x & 0x1 )
+                {/* when x is odd */
+                        if ( y & 0x1 )
+                        {/* when x and y are both odd */
+                                y = ( x - y ) >> 1;
+                                x -= y;
+                        }
+                        else
+                        {/* when x is odd and y is even */
+                                y >>= 1;
+                        }
+                }
+                else
+                {/* when x is even */
+                        if ( y & 0x1 )
+                        {/* when x is even and y is odd */
+                                x >>= 1;
+                                if ( x < y )
+                                {
+                                        temp = x;
+                                        x = y;
+                                        y = temp;
+                                }
+                        }
+                        else
+                        {/* when x and y are both even */
+                                x >>= 1;
+                                y >>= 1;
+                                ++factor;
+                        }
+                }
+        }
+        return ( x << factor );
+}
+
+
+int main()
+{
+      int i;     
+      int a[30];
+      for(i=0;i<30;i++)
+     {
+             a[i]=rand()%100 + 1;
+             printf("%d ",a[i]);
+      }
+       printf("\n");
+       int b[30];
+       for(i=0;i<30;i++)
+      {
+         b[i]=rand()%100 + 1;
+         printf("%d ",b[i]);
+      }
+      printf("\n");
+      clock_t start,finish;
+      double dur;
+      start= clock();
+      for(i=0;i<30;i++)
+    {
+        //printf("辗转相除法所得最大公约数为：%d\n",gcd(a[i],b[i]));
+        //printf("穷举法所得最大公约数为：%d\n",divisor(a[i],b[i]));
+          printf("更相减损法所得最大公约数为：%d\n",gcd2(a[i],b[i]));
+        //printf("Stein算法所得最大公约数为：%d\n",Stein(a[i],b[i]));
+    }
+ finish=clock();
+ dur=(double)(finish-start)/CLOCKS_PER_SEC;
+ printf("运行所用的时间为：%lf s\n",dur); 
+    return 0;
+ }
--- algorithm-initial/rotate.c
+++ algorithm-initial/rotate.c
@@ -1,5 +1,101 @@
 #include <stdio.h>
 
-void rotate( int* nums, int numsSize, int k ){
+void printNums( int * nums, int numsSize ){
+    printf( "[" );
+    for( int i = 0; i < numsSize; i++ ){
+	printf( "%d ", nums[i] );
+    }
+    printf( "]\n" );
     return;
+}
+
+/*
+ * 无算法实现
+ */
+void rotate( int* nums, int numsSize, int k ){
+    int i,j, temp = 0;
+
+    if( NULL == nums || 2 > numsSize ||  0 == k ){
+	return;
+    }
+
+    for( i = 0; i < k; i++ ){
+	temp = nums[numsSize - 1];
+	for( j = numsSize - 1; j > 0; j-- ){
+            nums[j] = nums[j-1];
+	}
+	nums[0] = temp;
+    }
+
+    return;
+}
+
+/*
+ * 逆序后再分段逆序
+ */
+void reverse( int* nums, int start, int end ){
+    int i, j, temp = 0;
+
+    for( i = start; i < (end - start)/2; i++ ){
+        temp = nums[i];
+	j = end - i - 1;
+	nums[i] = nums[j];
+	nums[j] = temp;
+    }
+}
+void rotate1( int* nums, int numsSize, int k ){
+
+    if( NULL == nums || 2 > numsSize ||  0 == k ){
+	return;
+    }
+
+    reverse( nums, 0, numsSize );
+    printNums( nums, numsSize );
+
+    reverse( nums, 0, k );
+    printNums( nums, numsSize );
+
+    reverse( nums, k+1, numsSize );
+    printNums( nums, numsSize );
+
+    return;
+}
+
+void rotate2( int* nums, int numsSize, int k ){
+    int i, j, m, temp1, temp2 = 0;
+
+    for( i = 0,j = 0; i < gcd(k, numsSize); i++ ){
+	m = (j + k)%numsSize;
+	temp1 = nums[i];
+	while( m != i){
+            temp2 = nums[m];
+	    nums[m] = temp1;
+	    m = (m + k)%numsSize; 
+	    temp1 = temp2;
+	}
+
+        printNums( nums, numsSize );
+    }
+
+    return;
+}
+
+int main( int argc, char * argv[]){
+    int nums[] = {1, 2, 3, 4, 5, 6, 7};
+//    int nums[] = {-1,-100,3,99};
+
+    if( argc != 2 ){
+        printf( "USAGE arg k" );
+	exit(-1);
+    }
+    int k = atoi(argv[1]);
+    printf( "k[%d]\n", k );
+
+    printNums( nums, sizeof(nums)/sizeof(nums[0]) );
+
+    rotate2( nums, sizeof(nums)/sizeof(nums[0]), k );
+
+//    printNums( nums, sizeof(nums)/sizeof(nums[0]) );
+
+    return 0;
 }
