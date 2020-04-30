417a2b31310ff2543067657ea15c455471801075
JimmySenny <JimmySenny@126.com>
2020-05-01 00:40:46 +0800

    dfs

--- queue&stack/numIsland.c
+++ queue&stack/numIsland.c
@@ -1,39 +1,52 @@
 #include <stdio.h>
 
-void bfs( char** grid, int row, int col, int gridSize, int* gridColSize ){
-    grid[row][col] = '0';
+int direction[4][2] = { {-1, 0 },    /* 上 */
+                        { 1, 0 },    /* 下 */
+                        { 0,-1 },    /* 左 */
+                        { 0, 1 } };  /* 右 */
+
+void dfs( char** grid, int row, int col, int gridSize, int* gridColSize ){
+
+    printf ( "dfs_grid[%d]\n", grid[row][col] );
+    if( 1 != grid[row][col] ){
+        return;
+    }
+
+    grid[row][col] = 2;
 
     /* 上方 */
-    if( 0 < row && '1' == grid[row - 1][col] ){
-        bfs(grid, row-1, col, gridSize, gridColSize);
+    if( 0 < row && 1 == grid[row - 1][col] ){
+        dfs(grid, row-1, col, gridSize, gridColSize);
     }
     /* 下方 */
-    if( gridSize < row + 1 && '1' == grid[row + 1][col] ){
-        bfs(grid, row+1, col, gridSize, gridColSize);
+    if( gridSize < row + 1 && 1 == grid[row + 1][col] ){
+        dfs(grid, row+1, col, gridSize, gridColSize);
     }
     /* 左方 */
-    if( 0 < col && '1' == grid[row][col-1] ){
-        bfs(grid, row, col-1, gridSize, gridColSize);
+    if( 0 < col && 1 == grid[row][col-1] ){
+        dfs(grid, row, col-1, gridSize, gridColSize);
     }
     /* 右方 */
-    if( gridColSize[row] > col+ 1 && '1' == grid[row][col+1] ){
-        bfs(grid, row, col+1, gridSize, gridColSize);
+    if( gridColSize[0] > col+ 1 && 1 == grid[row][col+1] ){
+        dfs(grid, row, col+1, gridSize, gridColSize);
     }
 
-
+    return;
 }
-int numIsland( char** grid, int gridSize, int* gridColSize ){
+int numIsland( char **grid, int gridSize, int* gridColSize ){
     int num = 0;
 
+    printf ( "grid_i_j[%d]\n", grid[0][0] );
     for( int i = 0; i < gridSize; i++ ){
-        for( int j = 0; j < gridColSize[i]; j++ ){
-            printf('[%s]\n', grid[i][j] );
-            if( '1' == grid[i][j] ){
+        for( int j = 0; j < gridColSize[0]; j++ ){
+            printf ( "j[%d]\n", j );
+            if( 1 == grid[i][j] ){
                 num++;
-                bfs(grid, i, j, gridSize, gridColSize);
+                dfs(grid, i, j, gridSize, gridColSize);
             }
         }
     }
 
+    printf ( "num[%d]\n", num );
     return num;
 }
--- queue&stack/tst.c
+++ queue&stack/tst.c
@@ -11,9 +11,30 @@ int main( int argc, char* argv[] ){
                      { '0','0','0','0','0'}  \
                     };
                     */
-    char grid[3][3] = { {'1','0','0'},{'0','1','0'},{'0','0','1'}};
+//    char grid[3][3] = { {'1','0','0'},{'0','1','0'},{'0','0','1'} };
+//    char grid[3][3] = { '1','0','0','0','1','0','0','0','1' };
+//    char grid[3][3] = { {"100"},{"010"},{"001"} };
+//    char grid[][3] = { "100","010","001" };
+    char grid[][3] = { {1,0,0}, {0,1,0}, {0,0,1} };
+    char* p[3];
+//    p[0] = &grid[0][0];
+    p[0] = grid[0];
+    p[1] = grid[1];
+    p[2] = grid[2];
 
-    printf( "[%d]\n", numIsland( grid, 3, grid[1] ) );
+//    printf( "row[%d]\n", strlen(p) );
+    printf( "col[%d]\n", *p[0] );
+
+    int tmp = 3;
+    printf( "[%d]\n", numIsland( p, 3, &tmp) );
+
+    for( int i = 0; i < 3; i++ ){
+        for( int j = 0; j < 3; j++ ){
+            printf( "grid[%d]", grid[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\n");
 
     return 0;
 }
