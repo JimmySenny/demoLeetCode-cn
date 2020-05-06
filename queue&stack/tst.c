22a6b1472aa27ed9cabffe618a227b8381726e86
JimmySenny <JimmySenny@126.com>
2020-05-06 21:43:09 +0800

    numIsland

--- queue&stack/tst.c
+++ queue&stack/tst.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 extern int numIsland( char** grid, int gridSize, int* gridColSize );
 
@@ -15,22 +16,34 @@ int main( int argc, char* argv[] ){
 //    char grid[3][3] = { '1','0','0','0','1','0','0','0','1' };
 //    char grid[3][3] = { {"100"},{"010"},{"001"} };
 //    char grid[][3] = { "100","010","001" };
-    char grid[][3] = { {1,0,0}, {0,1,0}, {0,0,1} };
+    char grid[][3] = { {'1','0','0'}, {'0','1','0'}, {'0','0','1'},{'0','0','0'} };
     char* p[3];
 //    p[0] = &grid[0][0];
     p[0] = grid[0];
     p[1] = grid[1];
     p[2] = grid[2];
+    p[3] = grid[3];
 
-//    printf( "row[%d]\n", strlen(p) );
-    printf( "col[%d]\n", *p[0] );
+    char grid2[][5] = { {'1','1','1','1','0'}, \
+                        {'1','1','0','1','0'}, \
+                        {'1','1','0','0','0'}, \
+                        {'0','0','0','0','0'} };
+    char* p2[4];
+    // 指针必须赋值完全
+    p2[0] = grid2[0];
+    p2[1] = grid2[1];
+    p2[2] = grid2[2];
+    p2[3] = grid2[3];
 
-    int tmp = 3;
-    printf( "[%d]\n", numIsland( p, 3, &tmp) );
+    printf( "[%d-%d]\n", strlen(p),strlen(p[0]) ); //6, 4  6 TODO???
+    printf( "[%d-%d]\n", strlen(p2),strlen(p2[0]) ); //6, 4  6 TODO???
 
-    for( int i = 0; i < 3; i++ ){
-        for( int j = 0; j < 3; j++ ){
-            printf( "grid[%d]", grid[i][j]);
+    int tmp = 5;
+    printf( "[%d]\n", numIsland( p2, 4, &tmp) );
+
+    for( int i = 0; i < 4; i++ ){
+        for( int j = 0; j < 5; j++ ){
+            printf( "[%d]", grid2[i][j]);
         }
         printf("\n");
     }
