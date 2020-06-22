7af160f05046516a1892a14013c18c5c94b735a4
jimmysenny <jimmysenny@126.com>
2020-06-22 18:24:04 +0800

    longestPalindrome

--- dataStructure/src/string/longestPalindrome.c
+++ dataStructure/src/string/longestPalindrome.c
@@ -1,7 +1,39 @@
 #include "ds_string.h"
 
-char * longestPalindrome(char * s){
-    char* p = NULL;
+/*
+ * 暴力
+ */
+bool isPalindrome( const char * s ){
+    int len = strlen( s );
 
-    return p;
+    for ( int i = 0; i < len / 2; i++ ){
+        if( s[i] != s[len - i - 1]){
+            return false;
+        }
+    }
+
+    return true;
 }
+char * longestPalindrome(char * s){
+    int i = 0, j = 0;
+    int len = strlen( s );
+    char buf[1000+1];
+    int maxlen = 0;
+
+    for(i = 0; i < len; i++ ){
+        memset( buf, 0x00, sizeof( buf ) );
+        for( j = i + 1; j <= len; j++ ){
+            strncpy( buf, s + i, j - i );
+            //printf( "buf[%s]\n", buf );
+
+            if( isPalindrome( buf ) && ( j - i ) > maxlen ){
+                maxlen = j - i;
+                printf( "(i-j:%d-%d)[%s]\n", i, j, buf );
+            }
+        }
+    }
+
+    return buf;
+}
+
+
--- dataStructure/src/tst/tst_string.c
+++ dataStructure/src/tst/tst_string.c
@@ -1,6 +1,13 @@
 #include "ds_string.h"
 
 void tst_longestPalindrome( void ){
+    char a[] = "abcde";
+    char b[] = "xxabay";
+    char c[] = "xxabbay";
+
+    longestPalindrome( a );
+    longestPalindrome( b );
+    longestPalindrome( c );
 
     return;
 }
